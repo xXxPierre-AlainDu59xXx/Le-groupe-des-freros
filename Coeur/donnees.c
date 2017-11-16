539ad89fbe57cd0d866de0f15f1186707cfac942
xXxPierre-AlainDu59xXx <32566180+xXxPierre-AlainDu59xXx@users.noreply.github.com>
2017-11-16 19:15:42 +0100

    Update donnees.c

--- Coeur/donnees.c
+++ Coeur/donnees.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void ouvrirFichier(int *f)
+#include "donnees.h"
+
+FILE* openFile()
 {
-    f=fopen("Battements.csv","r");
+    // Pointer on my csv file
+    FILE* content = NULL;
+    content = fopen("info.csv", "r");
+
+    return content;
 }
 
-void fermerFichier(int *f)
+void getSize(FILE* content, int* numberLine)
 {
-    fclose(f);
+    char test;
+
+    while((test = getc(content)) != EOF)
+    {
+        if(test == '\n')
+        {
+            *numberLine = *numberLine+1;
+        }
+    }
 }
 
-void lireFichier(int *f)
+Value* creatingTab(int* numberLine)
 {
-    fclose(f);
+    // Declaration of my dynamic array that will contain the values ??of the csv file
+    Value* tab = NULL;
+    tab = malloc(*numberLine * sizeof(Value));
+
+    return tab;
 }
+
+void getValue(Value *tab, FILE* content)
+{
+    // Value to test our location in the file
+    char test;
+
+    // Incrementing variable
