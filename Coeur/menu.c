7552f18cc5bb96fc8521dac8a1aa9e43841ad8ce
xXxPierre-AlainDu59xXx <32566180+xXxPierre-AlainDu59xXx@users.noreply.github.com>
2017-11-16 19:17:48 +0100

    Update menu.c

--- Coeur/menu.c
+++ Coeur/menu.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void afficherDonneesFichier()
-{
-    FILE *f=NULL;
-    ouvrirFichier(f);
-    lireFichier(f);
-    fermerFichier(f);
-}
+#include "menu.h"
 
-void quitter()
+void menuSort(&menuSortChoice)
 {
-    return 0;
+    int *menuChoice;
+    printf("Please choose a menu:\n1-Raw data display\n2-Organized data display\n3-Moment specific data search\n4-Time range specific data search\n5-Number of stored lines\n6-Highest and lowest pulse values\n");
+    scanf("%d",&menuChoice);
+    if (menuChoice==2)
+    {
+        int menuSortChoice;
+        printf("Please choose a command:\n1-Growing data display time-wise\n2-Growing data display value-wise\n3-Descending data display time-wise\4-Descending data display value-wise");
+        scanf("%d",&menuSortChoice);
+    }
 }
