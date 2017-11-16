4aa5d908453a1a02029c8b7f0935b4cdf4e49eae
xXxPierre-AlainDu59xXx <32566180+xXxPierre-AlainDu59xXx@users.noreply.github.com>
2017-11-16 14:05:23 +0100

    Update actions.h

--- Coeur/actions.h
+++ Coeur/actions.h
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void afficherDonneesFichier();
+
+void afficherDonneesCroissant();
+
+void afficherDonneesDecroissant();
+
+void afficherDonneesTemps();
+
+void afficherMoyennePouls();
+
+void afficherNombreLignes();
+
+void afficherPoulsMax();
+
+void afficherPoulsMin();
+
+void quitter();
+
+//TODO: Mettre les prototypes dans les bons headers
