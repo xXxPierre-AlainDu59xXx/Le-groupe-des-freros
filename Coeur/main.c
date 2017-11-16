a9dcbfc41150e2408e47fef3f14577a50044c784
xXxPierre-AlainDu59xXx <32566180+xXxPierre-AlainDu59xXx@users.noreply.github.com>
2017-11-16 16:15:58 +0100

    Update main.c

--- Coeur/main.c
+++ Coeur/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+#include "actions.h"
+#include "donnees.h"
+#include "menu.h"
+
+int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
-    return 0;
+    quitter();
 }
