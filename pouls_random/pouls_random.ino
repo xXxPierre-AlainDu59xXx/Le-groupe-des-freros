d420205f453a637e2517f44261f720f777404e51
Wolfighht <33147094+Wolfighht@users.noreply.github.com>
2017-11-17 09:21:54 +0100

    Add files via upload
    ouvre le programme arduino, téléverse, lance att une 10ene de valeur ferme lance le processing laisse qq temps le carré fonctionner puis appuie sur entrer

---
+++ pouls_random/pouls_random.ino
@@ -0,0 +1,12 @@
+int pouls = random(70,85);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(1200);
+  pouls = random(pouls-1,pouls+2);
+  Serial.print(pouls);
+  Serial.println(";");
+}
