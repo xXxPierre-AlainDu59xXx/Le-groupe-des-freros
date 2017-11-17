064552fb78deb0f1920685ba0fbc1e78a94d3e91
xXxPierre-AlainDu59xXx <32566180+xXxPierre-AlainDu59xXx@users.noreply.github.com>
2017-11-17 09:34:18 +0100

    Add files via upload

---
+++ main/cardio.c
@@ -0,0 +1,13 @@
+#include "cardio.h"
+const int SORTIE=0; // La macro pour le pin de sortie
+
+void calculPulsation(int pouls, int battement) {
+  for(int temps=0;temps!=600;temps++) { //On teste le programme pendant 6 secondes (en fonction du delay en dessous)
+    delay(10); // On teste le programme toutes les 0.01 secondes
+    battement=analogRead(SORTIE); // La valeur en sortie est stockée dans "battement" (égal à 0 ou 1)
+    if (battement==1) { // Si "battement" = , on incrémente  "pouls"
+      pouls++;
+    }
+  }
+}
+
